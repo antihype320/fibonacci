41eec3d70ed8ea89ab2c39659442111e7f9c4a41
antihype320 <132342878+antihype320@users.noreply.github.com>
2023-09-19 19:28:56 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main
 		int n0 = 1, n1 = 1, n2;
 		System.out.println(n0);
 		System.out.println(n1);
-		for(int i = 3; i <= 11; i++) {
+		for(int i = 3; i <= 5; i++) {
 		    n2 = n0 + n1;
 		    System.out.println(n2);
 		    n0 = n1;
